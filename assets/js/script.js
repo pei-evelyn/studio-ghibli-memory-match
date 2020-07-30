7d7237a58bcc36b988f7a247a2c09fb35165e0bd
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-07-30 12:43:42 -0700

    Added game resets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var gameCardsContainer = document.querySelector('#gameCards');
+var resetButton = document.querySelector('#reset-button');
 
 var firstCardClicked = null;
 var secondCardClicked = null;
@@ -10,6 +11,8 @@ var matches = 0;
 
 var attempts = 0;
 var gamesPlayed = 0;
+
+// Handle Click
 
 gameCardsContainer.addEventListener('click', handleClick);
 
@@ -55,6 +58,8 @@ function handleClick(event) {
     }
 }
 
+// Stats Bar
+
 function displayStats() {
     var gamesPlayedNum = document.querySelector('#games-played-number');
     var attemptsNum = document.querySelector('#attempts-number');
@@ -66,6 +71,30 @@ function displayStats() {
 }
 
 function calculateAccuracy(attempts, matches) {
+    if (!attempts) {
+        return '0%';
+    }
     var accuracyNum = matches / attempts;
     return Math.round(accuracyNum * 100) + '%';
 }
+
+// Reset Game
+
+function resetGame() {
+    attempts = 0;
+    matches = 0;
+    gamesPlayed++;
+    displayStats();
+    resetCards();
+    var modal = document.querySelector('.modal-container');
+    modal.classList.add('hidden');
+}
+
+function resetCards() {
+    var hiddenCards = document.querySelectorAll('.card-back');
+    for (var i = 0; i < hiddenCards.length; i++) {
+        hiddenCards[i].classList.remove('hidden');
+    }
+}
+
+resetButton.addEventListener('click', resetGame)
