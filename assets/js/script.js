501576db2a76ac9b19f2f7d7b2d3140cda76014b
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-07-29 17:02:02 -0700

    Added JS functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 var gameCardsContainer = document.querySelector('#gameCards');
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardClasses = null;
+var secondCardClasses = null;
+
 gameCardsContainer.addEventListener('click', handleClick);
 
 function handleClick(event) {
@@ -8,4 +13,31 @@ function handleClick(event) {
     }
     var cardClicked = event.target;
     cardClicked.className += ' hidden'
+
+    if (!firstCardClicked) {
+        firstCardClicked = cardClicked;
+        var firstCardClickedSib = firstCardClicked.previousElementSibling;
+        firstCardClasses = firstCardClickedSib.className;
+    } else {
+        secondCardClicked = cardClicked;
+        var secondCardClickedSib = secondCardClicked.previousElementSibling;
+        secondCardClasses = secondCardClickedSib.className;
+        gameCardsContainer.removeEventListener('click', handleClick);
+
+        if (firstCardClasses === secondCardClasses) {
+            gameCardsContainer.addEventListener('click', handleClick);
+            firstCardClicked = null;
+            secondCardClicked = null;
+        } else {
+            setTimeout(function(){
+                firstCardClicked.classList.remove('hidden');
+                secondCardClicked.classList.remove('hidden');
+                gameCardsContainer.addEventListener('click', handleClick);
+                firstCardClicked = null;
+                secondCardClicked = null;
+            }, 1500)
+
+        }
+
+    }
 }
